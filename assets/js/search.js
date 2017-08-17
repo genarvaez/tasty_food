6eac377aa2339678caa415f583cc9392da8498bf
cstapiarubio <cstapiarubio@gmail.com>
2017-08-17 16:32:25 -0300

    muestra de toda la info de restaurant y ademas de sus comparaciones, falta diseño y muestra particular

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $.ajax({
     url: "https://developers.zomato.com/api/v2.1/search?entity_id=83&entity_type=city",
     beforeSend: function( req ) {
-      req.setRequestHeader('user-key','ca35cc550fe071a39f8a293be54b11ed');
+      req.setRequestHeader('user-key','dced529441fdc0169b85c82a8c296a5b');
     },
     type: 'GET',
     datatype: 'JSON',
@@ -11,27 +11,33 @@ $(document).ready(function(){
   .done(function(response){
     console.log(response);
     response.restaurants.forEach(function(e){
-      console.log(e);
       var precio = e.restaurant.average_cost_for_two;
-      console.log(precio);
       var name = e.restaurant.name;
       var usd = e.restaurant.currency;
       var cocina = e.restaurant.cuisines;
       var img = e.restaurant.featured_image;
-      console.log(name);
-      console.log(usd);
-      console.log(cocina);
-      console.log(img);
-
       var localidad = e.restaurant.location.locality_verbose;
       var address = e.restaurant.location.address;
       var rating = e.restaurant.user_rating.aggregate_rating;
 
-      console.log(localidad);
-      console.log(address);
-      console.log(rating);
+      //Despliegue de info de cada restaurant al apretar la foto principal
+      //$(".responsive-img").on("click", function(){
       $(".info-rest").append("<div class='col s12 m12 info'><h4>" + name + "</h4><i class='small material-icons'>favorite_border</i><div class='direccion'><h5>Address</h5><p>"+address+ "</p></div><div class='precio'><h5>Price</h5><p>"+usd+ precio+ "</p></div><div class='rating'><h5>Rating</h5>"+ rating+"</div></div>")
-    });
+    //});
+
+/*$(".historial").click(function(){
+        $(".send").empty();
+        $(".actual-profile").attr('src', $(this).children('div').children("img").attr("src"));
+        console.log($(this).children('div').children(".bold").text())
+        $(".actual-nick").html($(this).children('div').children(".bold").text())
+        $("span").html(miHora());
+    })*/
+
+      //Comparación dos restaurants
+      //$(".small").on("click", function(){
+      $(".info-rest").append("<div class='col s4 m4 comp'><h5>Cuisine</h5><h5>Cost for two</h5><h5>Rate</h5></div><div class='col s4 m4 res1'><h4>"+ name+ "</h4><p>"+cocina+ "</p><p>"+usd+ precio+"</p><p>"+rating+ "</p></div><div class='col s4 m4 res2'><h4>"+name+"</h4><p>"+cocina+ "</p><p>"+usd+ precio+"</p><p>"+rating+ "</p></div")
+      //});
+});
   })
 
   .fail(function(error){
