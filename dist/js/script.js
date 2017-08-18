12bb011a2a6d6dd8650dd6da1f476cbc771833ef
Kath Sandoval <katerinemarioly@gmail.com>
2017-08-18 10:08:10 -0300

    Intento de guardar imagen local-Storage

--- dist/js/script.js
+++ dist/js/script.js
@@ -20331,3 +20331,27 @@ $(function(){
 		$(".responsive-img").attr("src",img);
 	})
 })
+
+if(localStorage.img) { 
+debugger;
+$('#bannerImg').attr('src', localStorage.img);
+}
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+    localStorage.setItem('img', e.target.result);
+      $('#bannerImg').attr('src', reader.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(".file-upload").change(function() {
+  readURL(this);
+});
+
+$(".upload-button").on('click', function() {
+  $(".file-upload").click();
+});
