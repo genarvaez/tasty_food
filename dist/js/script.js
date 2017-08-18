2242e0b225e44fe9df3732345e9f044158ebbc4d
genarvaez <ge_naravez@hotmail.com>
2017-08-18 11:40:24 -0300

    subiendo demo inicial

--- dist/js/script.js
+++ dist/js/script.js
@@ -21142,6 +21142,8 @@ var data = [
   ]
 }]
 
+var miLatitud = [];
+$(document).ready(function(){
 $('select').material_select();
  $(".button-collapse").sideNav();
  $(".more-information").hide();
@@ -21179,7 +21181,7 @@ $(".select-city").on("change", function(){
 		entity_type: "city",
 	  },
 	})
-	.done(function(res) {
+	.done(function(res){
 		console.log("success");
 		console.log(res.restaurants)
 		$(".restaurant-box").html("");
@@ -21205,5 +21207,127 @@ $(".select-city").on("change", function(){
 
 })
 
+/*COMIENZA SECCION DISCOVER J*/
+
+  
+  var city;
+  $(".j-select").on("change", function(){
+    city = 83;
+  })
+  $.ajax({
+    url: "https://developers.zomato.com/api/v2.1/search",
+    beforeSend: function( req ) {
+      req.setRequestHeader("user-key", "7a3020c872c97e0a5c3589835c28da71");
+    },
+    type: "GET",
+    dataType: 'json',
+    data: {
+      entity_id: city,
+      entity_type: "city"    
+    },
+  })
+  .done(function(res) {
+    console.log("success");
+    console.log(res)
+    res.restaurants.forEach(function(e){
+      console.log(e)
+
+      miLatitud.push([e.restaurant.location.latitude + "," + e.restaurant.location.longitude]);
+      return miLatitud;
+    })  
+    console.log(miLatitud)
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  })
+});
+
+  //Funcion de geolocalizacion y trazado de ruta
+  function initMap(){
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+    var markers = [];
+    for (i = 0; i < miLatitud.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(miLatitud[i][0], miLatitud[i][1]),
+        map: map
+      });
+      markers.push(marker);
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  }
+/*TERMINA SECCION DISCOVER J*/
 
 
+
+
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+});
+
+
+$(function(){
+	$("#profile_image").change(function(e){
+		var img = URL.createObjectURL(e.target.files[0]);
+		$(".responsive-img").attr("src",img);
+	})
+})
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+            
+          document.getElementById('list').insertBefore(span, null);
+          localStorage.setItem('img', e.target.result);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+  if(localStorage.img) { 
+
+         var span = document.createElement('span');
+          span.innerHTML += ['<img class="thumb" src="', localStorage.img,
+                            '" title="test"/>'].join('');
+
+          document.getElementById('list').insertBefore(span, null);
+    
+    }
